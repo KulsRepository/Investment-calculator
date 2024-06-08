0980227ce70d1129824bb6eff6f6fcc898674201
Noe Hartman <kalimakuls@gmail.com>
2024-06-08 21:26:42 +0200

    summary
    added a library for the inflation rate, will make it better

--- investmentcalculator.py
+++ investmentcalculator.py
@@ -1,6 +1,7 @@
-inflationrate = 1.02
-Age = 29 #my actual age is 22 but I will start making decent money at 29 due to my PhD studies.
+import inflationrate as ir
+Age = int(input("Enter your age ")) #22
 year = 2002+29
+
 
 def calculator(init_inv, choose, next_inv, r, j):
 	calculator.i = 0
@@ -17,8 +18,8 @@ age: {Age+10*(multicalc.j-1)+calculator.i}, year: {year+10*(multicalc.j-1)+calcu
 	print(f"""the final investment amount after {calculator.i} years is {int(calculator.year)}$ USD
 	Total spent: {total_spent}
 	Total made: {calculator.year-total_spent}
-	The total amount is {int(calculator.year/(inflationrate**(calculator.i)))}$ USD in today's worth.
-	the total made is  {(calculator.year-total_spent)/(inflationrate**(calculator.i))}$ in today's worth.
+	The total amount is {int(calculator.year/(ir.inflationrate**(calculator.i)))}$ USD in today's worth. \
+	the total made is  {(calculator.year-total_spent)/(ir.inflationrate**(calculator.i))}$ in today's worth. \
 	My age at this point is {Age+calculator.i*multicalc.j}""")
 
 #this investment calculator calculates the same thing but the salary is adjusted for infation, this function is hence separate because not all jobs offer that.
@@ -30,7 +31,7 @@ def calculator2(init_inv, choose, next_inv, r, j):
 		calculator.i += 1
 		calculator.year = calculator.year*r
 		calculator.year = calculator.year + next_inv
-		next_inv = next_inv*inflationrate
+		next_inv = next_inv*ir.inflationrate
 		print(f"investment year: {calculator.i+10*(multicalc.j-1)}, new yearly investment input: {int(next_inv)}, \
 investment amount: {int(calculator.year)}, age: {Age+10*(multicalc.j-1)+calculator.i}, year: {year+10*(multicalc.j-1)+calculator.i}")
 	
@@ -52,7 +53,7 @@ def multicalc(init_inv, choose, next_inv, r, gos_at_it, roi_growth, income_growt
 		the new ratio is {r*roi_growth+multicalc.j*0.01}
 		the new yearly investment is {next_inv*income_growth*multicalc.j}
 		the {choose} year investment plan started with {calculator.year}
-																																																									""")
+																		""")
 		calculator(calculator.year, choose, next_inv*income_growth*multicalc.j, r*roi_growth, multicalc.j)
 #I know r*roi_growth only tops up once but It's actually better this way.		
 
@@ -60,4 +61,4 @@ def multicalc(init_inv, choose, next_inv, r, gos_at_it, roi_growth, income_growt
 multicalc(100000, 10, 50000, 1.08, 3, 1.01, 1.5)
 
 print(f"""I will have {int(calculator.year)}$ USD at the age of {Age+multicalc.j*calculator.i}
-That is worth {int(calculator.year/(inflationrate**(multicalc.j*calculator.i)))}$ USD now.""")
+That is worth {int(calculator.year/(ir.inflationrate**(multicalc.j*calculator.i)))}$ USD now.""")
